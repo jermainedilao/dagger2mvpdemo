b43c107a78a9d3201dcaab7d22a3cb03e260cae1
Jermaine Dilao <jermaine@sym.ph>
2017-02-11 20:30:59 +0800

    Updated app component

--- app/src/main/java/com/jermaine/dagger2/di/AppComponent.java
+++ app/src/main/java/com/jermaine/dagger2/di/AppComponent.java
@@ -6,16 +6,10 @@ import com.jermaine.dagger2.view.MainPresenterImpl;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import retrofit2.Retrofit;
 
 @Singleton
 @Component(modules = AppModule.class)
 public interface AppComponent {
-
-    Retrofit retrofit();
-
-    RetrofitRepositoryImpl retrofitRepositoryImpl();
-
     void inject(MainPresenterImpl mainPresenter);
 
     void inject(RetrofitRepositoryImpl retrofitRepository);
